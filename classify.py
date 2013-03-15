8a6ebcdaed522ef878a06f5be61e1c9dd2f449be
Srinidhi J <srinidhj.21@gmail.com>
2013-03-15 23:03:55 +0530

    Modify

--- classify.py
+++ classify.py
@@ -3,178 +3,96 @@ import re
 from collections import defaultdict
 from nltk.corpus import stopwords 
 from random import shuffle
-from nltk.stem.snowball import EnglishStemmer
 from nltk.tag import pos_tag
-from nltk.tokenize import word_tokenize
 import multiprocessing
 
-stemmer = EnglishStemmer()
+swords = []
 def getstopwords():
-	a = stopwords.words('english') + ['one','two','amp','best','the','once','twice','nine','it\'s']
-	return a 
+	global swords
+	swords = stopwords.words('english') + ['one','two','amp','best','the']
+getstopwords()
 
-def proc(a,flag=0):
-	'''process text : takes the entire file content as 
-	input and returns a list of form [{twitid:'2352514364257',category:'Politics',
-	data:['fasd','asdf','g', 'dfg','dsg ']}.....]'''
-	#flag = 0 only for training data
-	a = a.replace('\\u2019','\'')
-	a = a.lower().split('\n') #a is a list of tweets
-	res = []
-	pool = multiprocessing.Pool()
-	for i in a:
-		pool.apply_async(processor, (i,), callback=res.append)
-	pool.close()
-	pool.join()
-	if flag ==0 :
-		print 'Training done'
-	return res
-
-def processor(i,flag=0):
-	d = {}	
-	i = i.split()
-	i = [w for w in i if w not in getstopwords()]
-
-	d['twitid'] = i[0]
-	if flag == 0:
-		#Training data
-		d['category'] = i[1]
-		del(i[0])
-		del(i[0])
-	else:
-		del(i[0])
-	j = 0
-	while j < len(i):
-		i[j] = i[j].strip(r"!\$%\^&\*();:\?'\"\.,-_")
-		i[j] = i[j].replace('\\u2019','\'')
-		if re.match(r"(http://)|(rt)|(\\u2013)",i[j]):
-			del(i[j])
-		j += 1
-	d['data'] = [stemmer.stem(word).encode('ascii') for word in i]
-	d['word2']= [(a,b) for a in d['data'] for b in d['data'] if a!=b and pos_tag([a])[0][1] == 'NN' and pos_tag([b])[0][1] == 'NN']
-	return d
+def proc(twitdat,flag=0):
+	twitdat = twitdat.replace('\\u2019','\'')
+	twitdat = twitdat.lower().split('\n')
+	result = []
+	for dat in twitdat :
+		if len(i) < 3 :
+			return 
+		tempdict = {}	
+		dat = dat.split()
+		dat = [w for w in i if w not in swords]
+		tempdict['twitid'] = i[0]
+		if flag == 0:
+			tempdict['category'] = i[1]
+			del(dat[0])
+			del(dat[0])
+		else:
+			del(dat[0])
+		j = 0
+		while j < len(dat):
+			dat[j] = dat[j].strip(r"!\$%\^&\*();:'\"\.,-_\?")
+			if re.match(r"(http://)|(rt)",dat[j]):
+				del(dat[j])
+			j += 1
+		tempdict['data'] = i
+		result.append(tempdict)
+		return tempdict
+	return result
 
 def train(data):
-	'''Creates a list of features of the form [{word:'asdfba',count:{politics:10,sports:0}}]'''
-	r = []
-	for d in data:
-		for a in d['data']:
-			f = {}
-			g = {}		
+	result = []
+	for tdat in data:
+		for a in tdat.get('data',None):
+			fdict = {}
+			gdict = {}		
 			for temp in r:
 				if temp['word'] == a:
-					if temp['count'].get(d['category'],None):
-						temp['count'][d['category']] += 1
+					if temp['count'].get(tdat['category'],None):
+						temp['count'][tdat['category']] += 1
 					else:
-						temp['count'][d['category']] = 1
+						temp['count'][tdat['category']] = 1
 					break
 			else:
-				f['word'] = a 
-				if g.get(d['category'],None):
-					g[d['category']] += 1
+				fdict['word'] = a 
+				if gdict.get(tdat['category'],None):
+					gdict[tdat['category']] += 1
 				else:
-					g[d['category']] = 1   
-				f['count'] = g
-				r.append(f)
-	for d in data:
-		for a,b in d['word2'] :
-			f = {}
-			g = {}
-			flag = 1
-			for temp in r:
-				if temp.get('word2',None) == (a,b):
-					if temp['count2'].get(d['category'],None):
-						temp['count2'][d['category']] += 1
-						flag = 0
-					else:
-						temp['count2'][d['category']] = 1
-						flag = 0 
-						break
-			if flag == 1:
-				f['word2'] = (a,b)
-				if g.get(d['category'],None) :
-					g[d['category']] += 1
-				else :
-					g[d['category']] = 1
-				f['count2'] = g
-				r.append(f)
-	return r			
+					gdict[tdat['category']] = 1   
+				fdict['count'] = gdict
+				result.append(f)
+	return result			
 
-def validate(w):
-	'''Validates based on word count present in features'''
-	f = open("features.txt","a")
+def validate(twit):
 	global features
-	pc = 0
-	sc = 0
-	w = w.lower().split()
-	w = [word for word in w if word not in getstopwords()]
-	ser = ''
-	something = [word for word in w if word in features.keys()]
-	for word in something:
-		if re.match(r"(#)|(@)",word): 
-			flag = 1
-		ser += '%s :: '%(word)
-		c = feature.get('count',None)
-		if c:
-			p = c.get('politics',0)
-			s = c.get('sports',0)
-			pc += p
-			sc += s
-			if flag == 1 :
-				pc += p*p
-				sc += s*s
-			print pc,sc,word
-	w2 = [(a,b) for a in w for b in w if a!=b and pos_tag([a])[0][1] == 'NN' and pos_tag([b])[0][1] == 'NN']
-	for feature in features :
-		for a,b in w2:
-			if (a,b) in feature['word2']:
-				c = feature.get('count2',None)
+	polc = 0
+	spoc = 0
+	twit = twit.lower().split()
+	for feature in features1:
+		for word in twit:
+			flag = 0 
+			if feature.get('word','') == word:
+				if re.match(r"(#)|(@)",word):
+					flag = 1
+				count = feature.get('count',None)
 				if c:
 					p = c.get('politics',0)
 					s = c.get('sports',0)
-					pc += p**5
-					sc += s**5
-					print pc,sc,word 
-	lis= [value for feature, value in features if featue=='word2' and value==(a,b)]
-	f.write(ser)
-	f.close()
-	if pc > sc :
+					polc += p
+					spoc += s
+					if flag == 1 :
+						polc += p**10
+						spoc += s**10
+	if polc > spoc :
 		return 'Politics'
 	else:
 		return 'Sports'
+
 trdata = proc(open('training.txt','r').read())
-a = len(trdata)*19/20
-shuffle(trdata)
-features = train(trdata[:a])
-c = 0
-nc = 0
-
-#print validate("With four quality bowlers who can hurl the ball consistently at 140+kph Australia need not really worry about the nature of the pitch...")
-'''
-print '#'*80
-print 'Word \t\tPolitics\t\tSports'
-print '#'*80
-features.sort()
-for f in features:
-	p = f['count']
-	print "%s\t\t%s\t\t%s\t\t"%(f['word'],p.get('politics',0),p.get('sports',0))
-
-st = ''
-for i in trdata[a:]:
-	if validate(' '.join(i['data'])) == i['category']:
-		c += 1
-	else:
-		nc += 1
-		st += '%s\t\t::%s\n'%(i['category'],' '.join(i['data']))
-		print i
-print c, nc
-cr = float(100*c)/float(c+nc)
-print 'correctness = %s' %(cr)
-f = open("notcorrect.txt","w")
-f.write(st)
-f.close()
-'''
-valdata = proc(open('validation.txt','r').read(),flag=1)
+a = len(trdata)
+features1= train(trdata)
+print features1
+valdata = proc(open('validation.txt','r').read())
 f = open('output.txt','w')
 for v in valdata:
 	st = v['twitid'] +'\t'+ validate(' '.join(v['data']))+'\n'
